5e9257bc2a2dde06883f5c85ec40a5203543fcd0
Erik Uecke <erik.uecke@digitalfilmacademy.edu>
2017-02-27 16:09:19 -0500

    Chapter18 TouchTracker

---
+++ Chapter18/TouchTracker18/TouchTracker18/DrawView.swift
@@ -0,0 +1,139 @@
+//
+//  DrawView.swift
+//  TouchTracker18
+//
+//  Created by Erik Uecke on 2/27/17.
+//  Copyright © 2017 Erik Uecke. All rights reserved.
+//
+
+import UIKit
+
+class DrawView: UIView {
+    
+//    var currentLine: Line?
+    var currentLines = [NSValue: Line]()
+    var finishedLines = [Line]()
+    
+    // Drawing with Drawview
+    func stroke(_ line: Line) {
+        let path = UIBezierPath()
+        path.lineWidth = lineThickness
+        path.lineCapStyle = .round
+        
+        path.move(to: line.begin)
+        path.addLine(to: line.end)
+        path.stroke()
+    }
+    
+    override func draw(_ rect: CGRect) {
+        
+        finishedLineColor.setStroke()
+        for line in finishedLines {
+            stroke(line)
+        }
+        
+        currentLineColor.setStroke()
+        for (_, line) in currentLines {
+            stroke(line)
+        }
+    }
+    
+    // Turning touches into lines
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        // Log statement to see the order of events
+        print(#function)
+        
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+            let newLine = Line(begin: location, end: location)
+            
+            let key = NSValue(nonretainedObject: touch)
+            currentLines[key] = newLine
+        }
+        
+        setNeedsDisplay()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        // Log satment to see the order of events
+        print(#function)
+        
+        for touch in touches {
+            let key = NSValue(nonretainedObject: touch)
+            currentLines[key]?.end = touch.location(in: self)
+        }
+        
+        setNeedsDisplay()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        // Log statment to see the order of events
+        print(#function)
+        
+        for touch in touches {
+            let key = NSValue(nonretainedObject: touch)
+            if var line = currentLines[key] {
+                line.end = touch.location(in: self)
+                
+                finishedLines.append(line)
+                currentLines.removeValue(forKey: key)
+            }
+        }
+        
+        setNeedsDisplay()
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // Log statement to see the order of events
+        print(#function)
+        
+        currentLines.removeAll()
+        
+        setNeedsDisplay()
+    }
+    
+    // @IBInspectable - for customizing color and thickness of lines
+    
+    @IBInspectable var finishedLineColor: UIColor = UIColor.black {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var currentLineColor: UIColor = UIColor.red {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var lineThickness: CGFloat = 10 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Chapter18/TouchTracker18/TouchTracker18/Line.swift
@@ -0,0 +1,16 @@
+//
+//  Line.swift
+//  TouchTracker18
+//
+//  Created by Erik Uecke on 2/27/17.
+//  Copyright © 2017 Erik Uecke. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+
+}
